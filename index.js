6d42498a247fd1a31b87ceec4cfef70b6b396c0a
BrandonWorobi <79031043+BrandonWorobi@users.noreply.github.com>
2021-06-09 14:42:59 -0400

    Task 2 & 3 Pass

--- index.js
+++ index.js
@@ -65,8 +65,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(array = originalFlavors) { 
+    if (originalFlavors.length !== 31) {
+        return false;
+    } else {
+        return true;
+   }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -82,8 +86,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(passingArray, newFlavor){
+    passingArray.unshift(newFlavor);
+    return passingArray
 }
 
 
